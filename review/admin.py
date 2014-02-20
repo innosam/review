4d5d9c5ea29efb3739aae60de5814ecf18a9ff63
PratAm <amarpratap.knit@gmail.com>
2014-02-20 19:14:53 +0530

    updated admin view

--- review/admin.py
+++ review/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from review.models import *
 
 admin.site.register(reviews)
+admin.site.register(node_user)
 admin.site.register(edge_friend)
 admin.site.register(app_context)
 # Register your models here.
